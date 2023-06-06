37853e7cb1427814d966e8d1eade95d7d26d2c84
gpaul988 <gpaul988@gmail.com>
2023-06-06 05:18:41 -0700

    Python-if_else_loops_functioins

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 # Graham S. Paul (3-print_alphabt.py)
 
-for i in range(97, 123):
-    if chr(i) != 'q' and chr(i) != 'e':
-        int("{}".format(chr(i)), end="")
+for i in range(ord('a'), ord('z') + 1):
+    if chr(i) != 'e' and chr(i) != 'q':
+        print('{:c}'.format(i), end='')

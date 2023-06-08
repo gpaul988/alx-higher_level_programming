fc7edc521ade3aead5b0e77323c2af761e3bd35b
gpaul988 <gpaul988@gmail.com>
2023-06-08 10:34:24 -0700

    update for 102-magic_calculation.py

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
 # Graham S. Paul (102-magic_calculation.py)
-
-"""Tie bytecode givem by Holberton School"""
-
 def magic_calculation(a, b):
+    """Tie bytecode given by Holberton School."""
     from magic_calculation_102 import add, sub
     if a < b:
         c = add(a, b)
         for i in range(4, 6):
             c = add(c, i)
-        return (c)
+            return (c)
     else:
         return(sub(a, b))

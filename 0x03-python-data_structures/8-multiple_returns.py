7d37ae50a702b762b808f23a5cc9b8d77d97bcc0
gpaul988 <gpaul988@gmail.com>
2023-06-12 19:26:47 -0700

    multiple returns fix

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 # Graham S. Paul (8-multiple_returns.py)
-
-"""Reverses length of string and its begining character"""
-
 def multiple_returns(sentence):
+    """Reverses length of string and its begining character"""
     if len(sentence) == 0:
         return (0, None)
     return (len(sentence), sentence[0])

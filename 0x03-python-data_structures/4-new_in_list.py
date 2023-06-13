9ee15cc084ee1f683d467c4d5d5426231cffa96a
gpaul988 <gpaul988@gmail.com>
2023-06-12 19:16:15 -0700

    new in list fix

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 # Graham S. Paul (4-new_in_list.py)
-
-"""Change element in a mimiced list at specific location"""
-
 def new_in_list(my_list, idx, element):
     if idx < 0:
         return my_list

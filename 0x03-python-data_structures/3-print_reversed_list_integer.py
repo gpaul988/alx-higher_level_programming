8f70a1a0a0ff7bf7dc9a810312f9d9e8cac60e72
gpaul988 <gpaul988@gmail.com>
2023-06-12 19:12:11 -0700

    print reversed list integer fix

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-#Graham S. Paul (3-print_reversed_list_integer.py)
+# Graham S. Paul (3-print_reversed_list_integer.py)
 
 def print_reversed_list_integer(my_list=[]):
-    """pull integers of  list in return order"""
-    if isinstance(my_list, list):
-        my_list.reverse()
-        for i in my_list:
-            print("{:d}".format(i))
+    if my_list is None:
+        return
+
+    for i in my_list[::-1]:
+        print("{:d}".format(my_list[i - 1]))
+        i -= 1

c936176a9fa95cb543849525f3cf875c0f7a1488
gpaul988 <gpaul988@gmail.com>
2023-06-12 19:21:44 -0700

    no c fix

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
 # Graham S. Paul (5-no_c.py)
-
-"""Erase all Characters 'c' and 'C' from string"""
-
 def no_c(my_string):
-    new_string = ""
-    for c in my_string:
-        if c.lower() != "c":
-            new_string += c
-    return new_string
+    s = f""
+    for i in my_string:
+        if i != 'c' and i != 'C':
+            s += f"{i}"
+    return s

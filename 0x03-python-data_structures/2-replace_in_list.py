dd45a290aeec84eb4c44eae7606209196ebcc97a
gpaul988 <gpaul988@gmail.com>
2023-06-12 18:59:39 -0700

    replace in list fix

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 # Graham S. Paul (2-replace_in_list.py)
+
 """Return element of list fixed position"""
 def replace_in_list(my_list, idx, element):
     if idx < len(my_list) and idx > -1:

38304523b8716d010137c50b8fa2ac0dc476025b
gpaul988 <gpaul988@gmail.com>
2023-06-12 18:43:48 -0700

    print list integer fix

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -2,10 +2,5 @@
 #Graham S. Paul (0-print_list_integer.py)
 
 def print_list_integer(my_list=[]):
-
-    """
-    Pull all integers of a list
-    """
-
     for i in my_list:
         print("{:d}".format(i))

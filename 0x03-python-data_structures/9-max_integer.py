b5bd5727a26ac342e3cc82faa55c9e1e98832b6a
gpaul988 <gpaul988@gmail.com>
2023-06-12 19:30:36 -0700

    max integer fix

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 # Graham S. Paul (9-max_integer.py)
-
-"""Search for the biggest integer of list"""
-
 def max_integer(my_list=[]):
+    """Serch for biggest integer of list"""
     if my_list is None or len(my_list) == 0:
         return None
     max = my_list[0]

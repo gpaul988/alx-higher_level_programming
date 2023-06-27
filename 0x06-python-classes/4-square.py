0f2aecc2f0d98b690b1027b6c09c796e9fa81725
gpaul988 <gpaul988@gmail.com>
2023-06-27 16:05:38 -0700

    4square @ python-classes and objects

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+# Graham S. Paul (4-square.py)
+"""Explain class square"""
+
+
+class Square:
+    """Shows a square"""
+    def __init__(self, size=0):
+        """Boot fresh square"""
+        self.size = size
+
+    @property
+    def size(self):
+        """Restoress current size of square"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Calibrate current size of square"""
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Restores current area of square"""
+        return self.__size ** 2

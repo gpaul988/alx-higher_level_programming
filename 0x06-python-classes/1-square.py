e745302bc2d784e6a79894b3f167b2ae5f7589a4
gpaul988 <gpaul988@gmail.com>
2023-06-27 15:46:55 -0700

    1square @ python-classes and objects

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# Graham S. Paul
+"""Explain a class square"""
+
+
+class Square:
+    """Show a square"""
+    def __init__(self, size=None):
+        """Boot Fresh square"""
+        self.__size = size

1609e95332bc39fc57387b97e5ea40b07eeaa618
gpaul988 <gpaul988@gmail.com>
2023-06-27 16:34:07 -0700

    6square @ python-classes and objects

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+# Graham S. Paul (6-square.py)
+"""Explains class square"""
+
+
+class Square:
+    """Shows Square"""
+    def __init__(self, size=0, position=(0, 0)):
+        """Boots fresh square"""
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        """Acquires contemporary size of square"""
+        return self.__size
+
+    @property
+    def position(self):
+        """Acquires contemporary position of square"""
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        """Calibrates contemporary position for square"""
+        if type(value) is not tuple or len(value) != 2 or \
+                type(value[0]) is not int or type(value[1]) is not int or \
+                value[0] < 0 or value[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+    @size.setter
+    def size(self, value):
+        """Calibrate contemporary size of square"""
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Restores contemporary square"""
+        return self.__size ** 2
+
+    def my_print(self):
+        """Pulls square using # taking into account the location"""
+        if not self.size:
+            print()
+            return
+
+        print("\n" * self.position[1], end="")
+        print("\n".join([" " * self.position[0] +
+                        "#" * self.size
+                         for i in range(self.size)]))

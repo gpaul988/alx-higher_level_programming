4e4ae9d7aafad79653ba632f474a7a195da8ee48
gpaul988 <gpaul988@gmail.com>
2023-07-03 05:58:38 -0700

    2rectangle @ More classes and objects

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -4,24 +4,17 @@
 Holds a rectangle class
 """
 
-
 class Rectangle:
-    """
-    Explainis rectangle class with a secured attribute width and height
-    """
     def __init__(self, width=0, height=0):
-        """ Boots rectangles """
         self.width = width
         self.height = height
 
     @property
     def width(self):
-        """ Achiever reverses width """
         return self.__width
 
     @width.setter
     def width(self, value):
-        """ Calibrator Claibrates width if int > 0 """
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
@@ -30,12 +23,10 @@ class Rectangle:
 
     @property
     def height(self):
-    """ Achiever reverses height """
         return self.__height
 
     @height.setter
     def height(self, value):
-        """ Calibrator claibrates height if int > 0 """
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
@@ -43,9 +34,7 @@ class Rectangle:
         self.__height = value
 
     def area(self):
-        """ Reverses width * height """
         return self.__width * self.__height
 
     def perimeter(self):
-        """ Reverses 2*width + 2*height or reverss if width or height is 0 """
-        return 2 * (self._width + self._height)
+        return 2 * (self.__width + self.__height)

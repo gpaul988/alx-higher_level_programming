ea454704305a6bd69881a4ee7685698f9b310a09
gpaul988 <gpaul988@gmail.com>
2023-07-10 15:50:34 -0700

    inheritance @ 10 square

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,33 +1,32 @@
 #!/usr/bin/python3
-# Graham S. Paul (10-square.py)
 """
-Explains the Square class that assunes from Rectangle.
+Module 10-square
+
+Contains parent class BaseGeometry
+with public instance method area and integer_validation
+
+Contains subclass Rectangle
+with instantiation of private attributes width and height, validated by parent,
+extends parent's area method and prints with __str__
+
+Contains subclass Square
+with instantiation of private attribute size, validated by superclass
 """
+
 
 Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
+    """inherits from Rectangle, who inherits from BaseGeometry
+    methods:
+        __init__(self, size)
     """
-    Constitute a square.
-    """
-
     def __init__(self, size):
-        """
-        Boots a Square example.
-
+        """initializes size
         Args:
-            size (int): The size of the square.
+            size (int): private
         """
         self.integer_validator("size", size)
-        self.__size = size
         super().__init__(size, size)
-
-    def __str__(self):
-        """
-        Retrieves a string constitution of the Square example.
-
-        Retrieves:
-            str: The string constitution of the Square.
-        """
-        return "[Square] {}/{}".format(self.__size, self.__size)
+        self.__size = size

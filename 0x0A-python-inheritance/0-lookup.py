20e29fe7baa3f9759351a77de81e0357e65ab463
gpaul988 <gpaul988@gmail.com>
2023-07-10 14:25:19 -0700

    inherritance

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# Graham S. Paul (0-lookup.py)
+"""
+Holds procedure for  lookup that retrieves list of object's
+"""
+
+
+def lookup(obj):
+    """Retrieves  list of object's assigned procedures"""
+    return dir(obj)

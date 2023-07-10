ab77b61590828606df70023e8c201e545cf97067
gpaul988 <gpaul988@gmail.com>
2023-07-10 15:30:03 -0700

    inheritance

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 # Graham S. Paul (101-add_attribute.py)
 """
-Holds purpose that includes fresh ascribed if feasible
+Holds purpose that includes fresh feature if feasible
 """
 
 
 def add_attribute(obj, attribute, value):
     """
-    Includes ascribed to object if feasible
+    Includes features to object if feasiible
     """
     if ('__dict__' in dir(obj)):
         setattr(obj, attribute, value)

54492860764f6a6260586c35f28acbab1128a4cc
gpaul988 <gpaul988@gmail.com>
2023-08-16 17:26:41 -0700

    average

---
+++ 0x0D-SQL_introduction/14-average.sql
@@ -0,0 +1,6 @@
+-- Graham S. Paul (14-average.sql)
+-- Inputs the score average of all records in the table
+-- result column name should be 'average'
+
+SELECT AVG(score) AS 'average'
+FROM second_table;

fe80f05d81a895310f6b77e6a1bd3bb46aa1cfde
gpaul988 <gpaul988@gmail.com>
2023-08-16 17:10:43 -0700

    top scores

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,5 @@
+-- Graham S. Paul (10-top_score.sql)
+-- Generate scripts to select entries by name and scores
+-- Top score should be at top
+
+SELECT score, name FROM second_table ORDER BY score DESC;

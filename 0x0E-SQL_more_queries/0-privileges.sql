06e417afd5734d4cafe971ca62fc1134103be4d4
gpaul988 <gpaul988@gmail.com>
2023-08-16 19:29:08 -0700

    complete

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,4 @@
 -- Graham S. Paul (0-privileges.sql)
 -- Generating scripts that lists all privileges of the MySQL
-SHOW GRANTS FOR user_0d_1@localhost;
-SHOW GRANTS FOR user_0d_2@localhost;
+SHOW GRANTS FOR 'user_0d_1'@'localhost'
+SHOW GRANTS FOR 'user_0d_2'@'localhost'

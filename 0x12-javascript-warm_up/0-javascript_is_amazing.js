09316779f06cdf13962debb45d33039ad1f64b70
gpaul988 <gpaul988@gmail.com>
2023-09-11 17:54:40 -0700

    fixing amazing javascript

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 # Graham S. Paul (0-javscript_is_amazing.js) - Creating constnt variable to print Javascript is amazing
 
-const myVar = 'JavaScript is amazing';
+const myVar = 'JavaScript is amazing'
 
-console.log(myVar);
+console.log(myVar)

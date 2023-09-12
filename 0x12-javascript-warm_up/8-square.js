f559615fb055c637072b794060b1280ad4cd54d8
gpaul988 <gpaul988@gmail.com>
2023-09-11 18:46:33 -0700

    warming up

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 // Graham S. Paul (8-square.js)
-const num = Math.floor(+process.argv[2]);
 
-if (isNaN(num)) {
+if (isNaN(process.argv[2])) {
   console.log('Missing size');
 } else {
-  for (let i = 0; i < num; i++) {
-    console.log('X'.repeat(num));
+  for (let i = 0; i < parseInt(process.argv[2]); i++) {
+    console.log('X'.repeat(parseInt(process.argv[2])));
   }
+}

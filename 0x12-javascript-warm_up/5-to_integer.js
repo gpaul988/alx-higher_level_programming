b8935167cfdb5ce76cb553949833036b11e7ae65
gpaul988 <gpaul988@gmail.com>
2023-09-11 19:21:36 -0700

    warming up

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
-//Graham S. Paul (5-to_integers.js)
-const num = +process.argv[2];
+// Graham S. Paul (5-to_integers.js)
 
-if (isNaN(num)) {
+if (isNaN(process.argv[2])) {
   console.log('Not a number');
 } else {
-  console.log('My number:', Math.floor(num));
+  console.log('My number: ' + parseInt(process.argv[2]));
 }

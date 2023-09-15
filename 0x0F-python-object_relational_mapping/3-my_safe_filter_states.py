834551404d38627f1d731c54cbfa9b09bbe10c95
gpaul988 <gpaul988@gmail.com>
2023-09-15 10:38:10 -0700

    filters

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+# Graham S. Paul (3-my_safe_filter_states.py)
+"""
+Reinstate same states; secure from MySQL injections
+# http://bobby-tables.com/python
+variables
+"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+
+    # connect to database
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3])
+
+    # create cursor to exec queries using SQL; match arg given
+    cursor = db.cursor()
+    sql_cmd = """SELECT *
+                 FROM states
+                 WHERE name=%s ORDER BY id ASC"""
+    cursor.execute(sql_cmd, (argv[4],))
+
+    for row in cursor.fetchall():
+        print(row)
+    cursor.close()
+    db.close()

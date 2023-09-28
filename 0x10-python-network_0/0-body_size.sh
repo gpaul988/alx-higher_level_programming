e0c010679993c097d21ffebad94b7acc843d7fee
gpaul988 <gpaul988@gmail.com>
2023-09-28 15:28:31 -0700

    curl body

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# Graham S. Paul (0-body_size.sh)
-# Pull curl detail size
-curl -sI "$1" | grep "Content-Length" | cut -d' ' -f2
+# Graham S. Paul (0-body_size.sh) - Pull curl detail size
+curl -sI "$1" | grep -i Content-Length | awk '{print $2}'

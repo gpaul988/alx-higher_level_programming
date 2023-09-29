2105e69f48599bdf617d70c5953094fd54de8f0c
gpaul988 <gpaul988@gmail.com>
2023-09-29 08:48:20 -0700

    completed

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -3,13 +3,17 @@
 """
 Python script that takes in a URL sends a request to the URL and displays the body
 """
-from sys import argv
+import sys
+import urllib.error
 import urllib.request
 
 
 if __name__ == "__main__":
+    url = sys.argv[1]
+
+    request = urllib.request.Request(url)
     try:
-        with urllib.request.urlopen(argv[1]) as response:
-            print(response.read().decode('utf-8'))
+        with urllib.request.urlopen(request) as response:
+            print(response.read().decode("ascii"))
     except urllib.error.HTTPError as e:
         print("Error code: {}".format(e.code))

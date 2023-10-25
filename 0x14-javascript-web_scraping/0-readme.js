19c6881087c4707bb8721e1edc7536cc8da3b9bf
gpaul988 <gpaul988@gmail.com>
2023-10-25 11:44:13 -0700

    commit

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,8 +1,9 @@
 #!/usr/bin/node
+# Graham S. Paul ( 0-readme.js)
 const fs = require('fs');
-# Graham S. Paul (0-readme.js)
-const file = process.argv[2];
-fs.readFile(file, 'utf-8', (err, data) => {
+
+const fileName = process.argv[2];
+fs.readFile(fileName, 'utf8', (err, data) => {
   if (err) {
     console.log(err);
   } else {

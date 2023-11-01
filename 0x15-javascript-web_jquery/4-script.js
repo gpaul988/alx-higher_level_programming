d7bf5e04c55f2a05463c29b5d8cdf89c148a407b
gpaul988 <gpaul988@gmail.com>
2023-11-01 14:21:11 -0700

    task 4 added

---
+++ 0x15-javascript-web_jquery/4-script.js
@@ -0,0 +1,10 @@
+// Graham S. Paul (4-script.js)
+$(document).ready(function changeRed () {
+  $('DIV#toggle_header').click(function toogleHeader () {
+    if ($('HEADER').attr('class') !== 'red') {
+      $('HEADER').attr('class', 'red');
+    } else {
+      $('HEADER').attr('class', 'green');
+    }
+  });
+});

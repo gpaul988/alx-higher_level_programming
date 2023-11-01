afec8bd63f245e8342aed8ea52b8eccc5bca5db7
gpaul988 <gpaul988@gmail.com>
2023-11-01 13:30:59 -0700

    added task 2

---
+++ 0x15-javascript-web_jquery/2-script.js
@@ -0,0 +1,6 @@
+// Graham S. Paul (2-script.js)
+$(document).ready(function textColor () {
+  $('DIV#red_header').click(function changeColor () {
+    $('HEADER').css({ color: '#FF0000' });
+  });
+});

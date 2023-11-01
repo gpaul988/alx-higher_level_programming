2429a8987df1f7c504b69c8e695c2989e9edf1c3
gpaul988 <gpaul988@gmail.com>
2023-11-01 14:43:35 -0700

    task 102 added

---
+++ 0x15-javascript-web_jquery/102-script.js
@@ -0,0 +1,9 @@
+// Graham S. Paul (102-script.js)
+$('document').ready(function () {
+  const url = 'https://www.fourtonfish.com/hellosalut/?';
+  $('INPUT#btn_translate').click(function () {
+    $.get(url + $.param({ lang: $('INPUT#language_code').val() }), function (data) {
+      $('DIV#hello').html(data.hello);
+    });
+  });
+});

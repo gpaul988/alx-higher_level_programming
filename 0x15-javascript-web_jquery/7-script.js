880fb6a35307cdbba2e5166162d703e1daa8e510
gpaul988 <gpaul988@gmail.com>
2023-11-01 14:30:38 -0700

    task 7 added

---
+++ 0x15-javascript-web_jquery/7-script.js
@@ -0,0 +1,6 @@
+// Graham S. Paul (7-script.js)
+$(document).ready(function getCharacter () {
+  $.get('https://swapi-api.hbtn.io/api/people/5/?format=json', function apiName (data) {
+    $('#character').text(data.name);
+  });
+});

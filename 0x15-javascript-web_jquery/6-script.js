557ef716daa2d38b27c24c2d189217f29439cdd8
gpaul988 <gpaul988@gmail.com>
2023-11-01 14:25:03 -0700

    task 6 added

---
+++ 0x15-javascript-web_jquery/6-script.js
@@ -0,0 +1,6 @@
+// Graham S. Paul (6-script.js)
+$(document).ready(function updateHeader () {
+  $('DIV#update_header').click(function onlyUpdateText () {
+    $('HEADER').text('New Header!!!');
+  });
+});
